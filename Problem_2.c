6f983f7f06a1d5174e00c2f254a530147e9b327d
spoorthi69 <20cse0094@gmail.com>
2021-02-17 11:38:18 +0000

    it is about using for loop to get fibonacci series.

--- Problem_2.c
+++ Problem_2.c
@@ -7,9 +7,16 @@
 #include<stdio.h>
 int main()
 {
-    int n;
+    int n, a=0, b=1, i,c;
     printf("Enter the value for n\n");
     scanf("%d", &n);
+    for(i=1; i<=n; i++)
+    {
+      printf("%d", a);
+      c= a+b;
+      a=b;
+      b=c;
+    }
 
     //Write your code here
     
