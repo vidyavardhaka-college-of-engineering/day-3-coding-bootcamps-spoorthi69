b3f4724a4158e87312cc087d2bcab5b708ddb3d6
spoorthi69 <20cse0094@gmail.com>
2021-02-17 11:55:33 +0000

    To identify whether the number is even or odd using 'if' or 'else' statements.

--- Problem_3.c
+++ Problem_3.c
@@ -2,12 +2,15 @@
 
 #include <stdio.h>
 int main() {
-    int number;
+    int n;
     printf("Enter an integer: ");
-    scanf("%d", &number);
+    scanf("%d", &n);
 
     // Write your code here
-    
+    if (n%2==0)
+    printf("The given number is Even");
+    else
+    printf("The given number is odd");
     
    
     return 0;
